6652684061012d14bb588ba0a72d17b27ea59de5
shubham1207 <63014724+shubham1207@users.noreply.github.com>
2021-09-27 23:33:09 +0530

    Add files via upload

---
+++ random_word.py
@@ -0,0 +1,6 @@
+from random import randint
+word_list=["True", "Fail", "MAths", "physics", "Gaana", "SPotify", "numeric", "speaker", "Music", "Robot", "RObot 2", "Home", "House", "Housefull", "Housefull2","Housefull3", "Book", "Mouse", "Keybord", "Danger"]
+selected_index=randint(0, len(word_list)-1)
+print(word_list[selected_index])
+
+
